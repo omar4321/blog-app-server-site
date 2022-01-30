2f447c0040db007d855e80762f3b6e88dab3c794
omar4321 <akib2000omar@gmail.com>
2022-01-31 00:29:23 +0600

    'go'

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = 'http://localhost:5000';
+const url = 'https://calm-stream-08879.herokuapp.com';
 
 let gfs;
 const conn = mongoose.connection;
